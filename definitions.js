3d31b9b69c9fb1aa75e27eaa5d7be8c3764c4a7d
QuangLN <VDG.Quang.LN@deheus.com>
2021-05-11 15:41:02 +0700

    commit

--- definitions.js
+++ definitions.js
@@ -15,19 +15,43 @@ const Sampler = new Tone.Sampler({
 
 
 const NOTES = ['C','C#/Db','D','D#/Eb','E','F','F#/Gb','G','G#/Ab','A','A#/Bb','B'];
+
+
 const MAJOR = [1,3,4.5];
 const MINOR = [1,2.5,4.5];
+
+const MAJOR_6 = [1,3,4.5,5.5]
+const MINOR_6 = [1,2.5,4.5,5.5]
+
 const MAJOR_7 = [1,3,4.5,6]
-const MAJOR_M7 = [1,3,4.5,6.5];
 const MINOR_7 = [1,2.5,4.5,6]
+
+const MAJOR_M7 = [1,3,4.5,6.5];
 const MINOR_M7 = [1,2.5,4.5,6.5];
+
+const MAJOR_9 = [1,3,4.5,6,8];
+const MINOR_9 = [1,2.5,4.5,6,8];
+
 const AUG = [1,3,5];
+const AUG7 = [1,3,5,6];
+
 const DIM = [1,2.5,4];
+const DIM7 = [1,2.5,4,6];
+
+const SUS_2 = [1,2,4.5];
+const SUS_4 = [1,3.5,4.5];
+
+const M7B5 = [1,2.5,4,6.5];
 
 function playSound(keys, delay){
 	Tone.loaded().then(() => {
 		Sampler.triggerAttackRelease(keys, delay);
 	})
+}
+
+function playChord(name, delay){
+	let keys = getKeysFromChord(name);
+	playSound(keys, delay);
 }
 
 function getKeyPosition(name){
@@ -47,42 +71,68 @@ function transpose(note, val){
 
 function getKeysFromChord(name){
 	let root = name[0];
-	if (name.length > 1){
-		if (name[1] == '#' || name[1] == 'b'){
-			root += name[1];
-		} else {
-			if (name[1] == 'm') { // minor
-				if (name.length > 2) {
-					// minor M7
-				}
-			} else if (name[1] == 'M') {
-				//M7
-			} else if (name[1] == 'd' || name[1] == 'D' || name[1] == '-') {
-				if (name[1] == '-'){
-					if (name.length > 2) {
-						// dim7
-					}
-				} else {
-					if (name.length > 4){
-						// dim 7
-					} else {
-						//dim
-					}
-				}
-			} else if (name[1] == 'a' || name[1] == 'A' || name[1] == '+') {
-				if (name[1] == '-'){
-					if (name.length > 2) {
-						// aug7
-					}
-				} else {
-					if (name.length > 4){
-						// aug7
-					} else {
-						// aug
-					}
-				}
-			}
-		} 
-		
+	if (name[1] == '#' || name[1] == 'b'){
+		root += name[1];
+		name = name.slice(2, name.length);
+	} else {
+		name = name.slice(1, name.length);
 	}
+	
+	let seq = null;
+	name = name.replace(' ', '');
+	if (name.length > 0){
+		switch(name) {
+			case 'm':
+				seq = MINOR;
+			break;
+			case '7': 
+				seq = MAJOR_7;
+			break;
+			case 'M7': 
+			case 'Maj7':
+				seq = MAJOR_M7;
+			break;
+			case 'm7': 
+				seq = MINOR_7;
+			break;
+			case 'mM7': 
+				seq = MINOR_M7;
+			break;
+			case 'aug': 
+			case '+':
+				seq = AUG;
+			break;
+			case 'dim': 
+			case '-':
+				seq = DIM;
+			break;
+			case '+7': 
+			case 'aug7': 
+				seq = AUG7;
+			break;
+			case '-7': 
+			case 'dim7': 
+				seq = DIM7;
+			break;
+			case 'sus2':
+				seq = SUS_2;
+			break
+			case 'sus4':
+				seq = SUS_4;
+			break
+			case 'm7b5':
+				seq = M7B5;
+			break;
+		}
+	} else {
+		seq = MAJOR;
+	}
+	
+	let notes = [];	
+	for (let i = 0; i < seq.length; i++){
+		let note = transpose(root, seq[i] - 1).split('/')[0] + '4';
+		notes.push(note);
+	}
+	
+	return notes;
 }
